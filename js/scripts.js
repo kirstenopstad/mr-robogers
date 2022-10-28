8ebfe84d3a965f5326dc30906e38795cef6fcc73
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-28 09:57:54 -0700

    Replaces elements with both a 2 and 3 with 'Won't you be my neighbor', passes test 8.

--- js/scripts.js
+++ js/scripts.js
@@ -15,13 +15,13 @@ function roboTranslateNums(num){
   const translatedArray = numsArray.map(function(num){
     // Cast to string and check for 1
     num = num.toString();
-    // Replace any elements that include a 1 with "Beep!"
+    // Replace any elements that include a 1 (and not a 2 or 3) with "Beep!"
     if (num.includes("1") && !num.includes("2") && !num.includes("3")) {
       return num = "Beep!";
-    // Replace any elements that include a 2 with "Boop!"
-    } else if (num.includes("2")){
+    // Replace any elements that include a 2 (and not a 3) with "Boop!"
+    } else if (num.includes("2") && !num.includes("3")){
       return num = "Boop!";
-    // Replace any elements that include a 3 (using UI Logic) with "Won't you be my neighbor?"
+    // Replace any elements that include a 3 with "Won't you be my neighbor?"
     } else if (num.includes("3")){
       return num = "Won't you be my neighbor?";
     } else {
@@ -31,9 +31,3 @@ function roboTranslateNums(num){
   
   return translatedArray;
 }
-  
-    // If element contains 2, implement 2 replacement
-    // If element contains 3, implement 3 replacement
-  // Replace any elements that include a 2 (using UI Logic) with "Boop!"
-    // If element contains 3, implement 3 replacement
-  // Replace any elements that include a 3 (using UI Logic) with "Won't you be my neighbor?"
