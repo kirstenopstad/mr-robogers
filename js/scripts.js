5f7252f1d80fc86cf7aa3483c0cb8f48fe113084
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-28 09:38:12 -0700

    Replaces any elements that include a 3 with 'Won't you be my neighbor?', passes test five.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 function roboTranslateNums(num){
   // Initialize numsArray
   const numsArray = [];
+  
   // Validate that user input is a number
   if (!Number.isInteger(num)) {
     return 0;
@@ -10,11 +11,29 @@ function roboTranslateNums(num){
       numsArray.push(i);
     };
   };
-  return numsArray;
-  // Replace any elements that include a 1 (using UI Logic) with "Beep!"
+  // For each number in array
+  const translatedArray = numsArray.map(function(num){
+    // Cast to string and check for 1
+    num = num.toString();
+    // Replace any elements that include a 1 with "Beep!"
+    if (num.includes("1")) {
+      return num = "Beep!";
+    // Replace any elements that include a 2 with "Boop!"
+    } else if (num.includes("2")){
+      return num = "Boop!";
+    // Replace any elements that include a 3 (using UI Logic) with "Won't you be my neighbor?"
+    } else if (num.includes("3")){
+      return num = "Won't you be my neighbor?";
+    } else {
+      return parseInt(num);
+    }
+  });
+  
+  return translatedArray;
+}
+  
     // If element contains 2, implement 2 replacement
     // If element contains 3, implement 3 replacement
   // Replace any elements that include a 2 (using UI Logic) with "Boop!"
     // If element contains 3, implement 3 replacement
   // Replace any elements that include a 3 (using UI Logic) with "Won't you be my neighbor?"
-}
