046adf64b27859e249c2996a1041c7c994c8988f
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-28 09:08:33 -0700

    Validates input is a number, passes test one

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function roboTranslateNums(num){
+  // Validate that user input is a number
+  if (!Number.nums) {
+    return 0;
+  };
+  // Create array from 0 to user input
+  // Replace any elements that include a 1 (using UI Logic) with "Beep!"
+    // If element contains 2, implement 2 replacement
+    // If element contains 3, implement 3 replacement
+  // Replace any elements that include a 2 (using UI Logic) with "Boop!"
+    // If element contains 3, implement 3 replacement
+  // Replace any elements that include a 3 (using UI Logic) with "Won't you be my neighbor?"
+}
